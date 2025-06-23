2fbcf0d6b89a5bf98088d1f5039181e9aafd0d77
MetalKaiser <metalkaiserpolanco@gmail.com>
2025-06-23 10:44:11 -0400

    HTML tags fixed

--- src/app/[locale]/(catalog)/components/home/Description.tsx
+++ src/app/[locale]/(catalog)/components/home/Description.tsx
@@ -5,13 +5,13 @@ export default function Descr() {
   const t = useTranslations("homeDescription");
 
   return(
-    <article className="h-[200px] md:h-[250px] flex justify-evenly items-center">
+    <section className="h-[200px] md:h-[250px] flex justify-evenly items-center">
       {descriptionImages.map((item, index) => (
-        <div key={index} className="w-3/12 md:w-4/12">
+        <article key={index} className="w-3/12 md:w-4/12">
           {item.icon}
           <p className="text-md dark:text-gray-400 text-gray-500 mt-1 text-center">{t(item.name)}</p>
-        </div>
+        </article>
       ))}
-    </article>
+    </section>
   );
 }
